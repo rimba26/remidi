652e506967f125fed027e690fda46ae028dc1c31
rimba26 <68635816+rimba26@users.noreply.github.com>
2020-10-08 21:34:44 +0700

    remidi finish

---
+++ remidi.php
@@ -0,0 +1,108 @@
+<?php
+$merk = [
+    ["Iphone", "Iphone 12 Pro Max", 42000000],
+    ["Iphone", "Iphone 11 Pro max", 24000000],
+    ["Samsung", "Samsung Galaxy Fold Z2", 34000000],
+    ["Xiaomi", "Xioami MI 10", 10000000],
+    ["Realme", "Realme X3 Zoom", 8000000],
+    ["Asus", "Asus ROG 3", 14000000]
+];
+
+// Membuat Pilihan
+$temp_arr=[];
+
+foreach ($merk as $key) {
+//    var_dump($key[0]);
+   $temp_arr[]=$key[0];
+}
+$pilihan=array_unique($temp_arr);
+// var_dump($pilihan);
+
+// End pilihan
+
+// Blok filter
+$tampilkan_merk=[];
+if(isset($_POST['filter']))
+{
+    // echo "tes";
+    // var_dump($_POST['filter']);
+    $filter=$_POST['filter'];
+    if($filter == "")
+    {
+        $tampilkan_merk=$merk;
+    }else{
+        foreach($merk as $key)
+        {
+            if($key[0] == $filter){
+                $tampilkan_merk[]=[$key[0],$key[1],$key[2]];
+            }
+        }
+    }
+}else{
+    $tampilkan_merk=$merk;
+}
+// var_dump($tampilkan_merk);
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<img src="logo-asus.png" class="rounded float-left" alt="logo" style="width: 170px;">
+<h1 style="text-align: center; padding-top:50px; color:silver; text-decoration:blink; font-family:italic;">Daftar Harga Hp Yang Baru Dirilis</h1>
+<marquee style="color:red;">Harga Dapat Berubah Setiap Saat!!!</marquee>
+    
+    <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">TYPE</th>
+      <th scope="col">MERK</th>
+      <th scope="col">HARGA</th>
+    </tr>
+  </thead>
+  <?php $grand_total=0; ?>
+  <?php foreach ($tampilkan_merk as $key => $value): ?>
+  <tbody>
+    <tr>
+      <td><?php echo $value[0]; ?></td>
+      <td><?php echo $value[1]; ?></td>
+      <td><?php echo $value[2]; ?></td>
+    </tr>
+</tbody>
+<?php $grand_total+=$value[2]; ?>
+<?php endforeach; ?>
+<thead class="thead-light">
+    <tr>
+      <th scope="col">Total Keseluruhan</th>
+      <th scope="col"></th>
+      <th scope="col"><?php echo $grand_total; ?></th>
+    </tr>
+  </thead>
+
+</table>
+<form action="remidi.php" method="post">
+        <select name="filter">
+            <option value="">
+                Tampilkan Semua
+            </option>
+            <?php foreach ($pilihan as $key): ?>
+                <option value="<?php echo $key; ?>"><?php echo $key; ?></option>
+            <?php endforeach; ?>      
+        </select>
+        <input type="submit" value="filter">
+    </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
